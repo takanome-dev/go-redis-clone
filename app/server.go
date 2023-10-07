256c66912ee3a4b0c63e9fb6d8e79c2dee29f7f9
takanome_dev <takanomedev221@gmail.com>
2023-10-07 22:25:35 +0000

    stuck in stage 4

--- app/server.go
+++ app/server.go
@@ -1,43 +1,69 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"io"
 	"log"
 	"net"
-	"os"
 )
+
+func handler(conn net.Conn) {
+	defer conn.Close()
+	// scanner := bufio.NewScanner(conn)
+	// for scanner.Scan() {
+	// 	line := strings.ToLower(scanner.Text())
+	// 	log.Printf("Received: %s\n", line)
+
+	// 	switch line {
+	// 	case "ping":
+	// 		conn.Write([]byte("+PONG\r\n"))
+	// 	case "echo":
+	// 		conn.Write([]byte("+Client sent an echo cmd\r\n"))
+	// 	default:
+	// 		conn.Write([]byte("+Default case hit\r\n"))
+	// 	}
+	// }
+	for {
+		buff := make([]byte, 512)
+		n, err := conn.Read(buff)
+	
+		if errors.Is(err, io.EOF) {
+			log.Printf("client %v closed a connection\n", conn.RemoteAddr())
+			return
+		}
+	
+		if err != nil {
+			log.Printf("error reading from connection: %v\n", err.Error())
+			return
+		}
+
+		log.Printf("capacity of the buffer is: %d", cap(buff))
+		// text := strings.ToUpper(string(buff[:n]))
+		// var response string
+		log.Printf("Received: %s\n", buff[:n])
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			log.Fatalf("error writing to connection: %v", err.Error())
+		}
+	}
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		log.Fatalf("Failed to bind to port 6379: %v", err.Error())
 	}
 
 	defer l.Close()
 	log.Printf("server listening at localhost %s\n", l.Addr())
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	defer conn.Close()
 	
 	for {
-		buff := make([]byte, 512)
-		n, err := conn.Read(buff)
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Printf("error reading from connection: %v\n", err.Error())
-			return
+			log.Fatalf("Error accepting connection: %v", err.Error())
 		}
-
-		fmt.Printf("Received: %s\n", string(buff[:n]))
-		_, err = conn.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			fmt.Printf("error writing to connection: %v\n", err.Error())
-			return
-		}
+		
+		go handler(conn)
 	}
 }
